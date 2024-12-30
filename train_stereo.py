23373d48c8a5024d5f7db529d3efd5b25669086d
Gangwei XU <gwxu@hust.edu.cn>
2024-12-30 14:04:55 +0800

    Update train_stereo.py

--- train_stereo.py
+++ train_stereo.py
@@ -222,7 +222,7 @@ if __name__ == '__main__':
     parser.add_argument('--restore_ckpt', default=None, help='load the weights from a specific checkpoint')
     parser.add_argument('--logdir', default='./checkpoints', help='the directory to save logs and checkpoints')
     parser.add_argument('--mixed_precision', default=True, action='store_true', help='use mixed precision')
-    parser.add_argument('--precision_dtype', default='float16', choices=['float16', 'bfloat16', 'float32'], help='Choose mixed precision type: float16 or bfloat16 or float32')
+    parser.add_argument('--precision_dtype', default='float16', choices=['float16', 'bfloat16', 'float32'], help='Choose precision type: float16 or bfloat16 or float32')
 
     # Training parameters
     parser.add_argument('--batch_size', type=int, default=8, help="batch size used during training.")
