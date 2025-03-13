a2e4c7b152f5428baeda71a26d112f11f0fb72dd
Gangwei XU <gwxu@hust.edu.cn>
2025-03-13 22:12:29 +0800

    Update geometry.py

--- core_rt/geometry.py
+++ core_rt/geometry.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 from core_rt.utils.utils import bilinear_sampler
 
 
-class Combined_Geo_Encoding_Volume:
+class Geo_Encoding_Volume:
     def __init__(self, geo_volume, num_levels=2, radius=4):
         self.num_levels = num_levels
         self.radius = radius
@@ -32,13 +32,3 @@ class Combined_Geo_Encoding_Volume:
         out = torch.cat(out_pyramid, dim=-1)
         return out.permute(0, 3, 1, 2).contiguous().float()
 
-    
-    @staticmethod
-    def corr(fmap1, fmap2):
-        B, D, H, W1 = fmap1.shape
-        _, _, _, W2 = fmap2.shape
-        fmap1 = fmap1.view(B, D, H, W1)
-        fmap2 = fmap2.view(B, D, H, W2)
-        corr = torch.einsum('aijk,aijh->ajkh', fmap1, fmap2)
-        corr = corr.reshape(B, H, W1, 1, W2).contiguous()
-        return corr
